7a68c3b6f8268cd2f59b4d68cbd53e15aa70ec69
Iuri Ostrikov <iurii84@gmail.com>
2019-05-25 13:44:31 -0400

    add SQL scripts

---
+++ data_filler.sql
@@ -0,0 +1,30 @@
+INSERT INTO `computer_parts` VALUES
+
+(1,  3,   false, 'Video Card'),
+(2,  14,  true,  'Mother-board'),
+(3,  75,  true,  'CPU'),
+(4,  29,  true,  'Casing'),
+(5,  5,   false, 'Mouse'),
+(6,  10,  false, 'Keyboard'),
+(7,  117, true,  'RAM'),
+(8,  30,  false, 'SSD'),
+(9,  178, false, 'HDD'),
+(10, 7,   false, 'Floppy'),
+(11, 179, false, 'CD_RW'),
+(12, 6,   false, 'Monitor'),
+(13, 3,   false, 'Wi-Fi dongle'),
+(14, 243, true,  'CPU Cooler'),
+(15, 12,  false, 'Card reader'),
+(16, 2,   false, 'Trackball'),
+(17, 5,   false, 'DVD-RW'),
+(18, 50,  false, 'Sound card'),
+(19, 345, false, 'Sata power adapter'),
+(20, 96,  true,  'Power Supply'),
+(21, 32,  false, 'Modem'),
+(22, 45,  false, 'Printer'),
+(23, 17,  false, 'Scanner'),
+(24, 65,  false, 'Webcam'),
+(25, 45,  false, 'Windows 10');
+
+
+
